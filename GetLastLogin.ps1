e99c59cdc15d2b55ae64f90e0f35c41587ee9cd6
Mifumi Ono <miono@microsoft.com>
2018-12-18 14:54:21 +0900

    Change so that it can also get with the key

--- GetLastLogin.ps1
+++ GetLastLogin.ps1
@@ -1,49 +1,53 @@
-Add-Type -Path ".\Tools\Microsoft.IdentityModel.Clients.ActiveDirectory\Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+Param(
+    [Parameter(ValueFromPipeline = $true,mandatory=$true)][ValidateSet("Cert", "Key")][String]$authMethod,
+    [Parameter(ValueFromPipeline = $true,mandatory=$true)][String]$clientSecretOrThumbprint
+)
 
-#
 # Authorization & resource Url
-#
 $tenantId = "contoso.onmicrosoft.com" 
 $resource = "https://graph.microsoft.com" 
 $clientID = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXX"
-$client_secret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 $outfile = "C:\Users\xxxxx\Desktop\lastLogin.csv"
-
-$data = @()
-$users= @()
-
-
-#
-# Authorization & resource Url
-#
 $authUrl = "https://login.microsoftonline.com/$tenantId/" 
+$data = @()
 
-#
-# Get certificate
-#
-$cert = Get-ChildItem -path cert:\CurrentUser\My | Where-Object {$_.Thumbprint -eq $client_secret}
+switch ($authMethod)
+{
+    "cert" {
+        Add-Type -Path ".\Tools\Microsoft.IdentityModel.Clients.ActiveDirectory\Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
 
-#
-# Create AuthenticationContext for acquiring token 
-# 
-$authContext = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext $authUrl, $false
+        # Get certificate
+        $cert = Get-ChildItem -path cert:\CurrentUser\My | Where-Object {$_.Thumbprint -eq $clientSecretOrThumbprint}
+    
+        # Create AuthenticationContext for acquiring token  
+        $authContext = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext $authUrl, $false
+    
+        # Create credential for client application 
+        $clientCred = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.ClientAssertionCertificate $clientID, $cert
+    
+        # Acquire the authentication result
+        $authResult = $authContext.AcquireTokenAsync($resource, $clientCred).Result
+        $accessTokenType = $authResult.AccessTokenType
+        $accessToken = $authResult.AccessToken
+    }
+    "key" {
+        $postParams = @{
+            client_id     = $clientID; 
+            client_secret = $clientSecretOrThumbprint;
+            grant_type    = 'client_credentials';
+            resource      = $resource
+        }
+        $authResult = (Invoke-WebRequest -Uri ($authUrl + "oauth2/token") -Method POST -Body $postParams) | ConvertFrom-Json
+        $accessTokenType = $authResult.token_type
+        $accessToken = $authResult.access_token
+    }
+}
 
-#
-# Create credential for client application 
-#
-$clientCred = New-Object Microsoft.IdentityModel.Clients.ActiveDirectory.ClientAssertionCertificate $clientID, $cert
-
-#
-# Acquire the authentication result
-#
-$authResult = $authContext.AcquireTokenAsync($resource, $clientCred).Result 
-
-
-if ($null -ne $authResult.AccessToken) {
+if ($null -ne $accessToken) {
     #
     # Compose the access token type and access token for authorization header
     #
-    $headerParams = @{'Authorization' = "$($authResult.AccessTokenType) $($authResult.AccessToken)"}
+    $headerParams = @{'Authorization' = "$($accessTokenType) $($accessToken)"}
     
     $reqUrl = "$resource/beta/users"
     do {
@@ -51,18 +55,16 @@ if ($null -ne $authResult.AccessToken) {
         $users += $rData.value
         $reqUrl = ($rData.'@odata.nextLink') + ''
     }while ($reqUrl.IndexOf('https') -ne -1)
-    
 
     $data += "UserPrincipalName,Last sign-in date in UTC (Last 30 days)"
-
     foreach ($user in $users) {
-        $reqUrl = "$resource/beta/auditLogs/signIns?&`$filter=userPrincipalName eq '" + [System.Web.HttpUtility]::UrlEncode($user.UserPrincipalName) + "'&`$orderby=createdDateTime desc &`$top=1"
+        $reqUrl = "$resource/beta/auditLogs/signIns?&`$filter=userId eq '" + $user.id + "'&`$orderby=createdDateTime desc &`$top=1"
         $rData = (Invoke-WebRequest -UseBasicParsing -Headers $headerParams -Uri $reqUrl).Content | ConvertFrom-Json
         $data += $user.UserPrincipalName + "," + $rData.value[0].createdDateTime
     }
+
+    $data | Out-File $outfile -encoding "utf8"
 }
 else {
     Write-Host "ERROR: No Access Token"
 }
-
-$data | Out-File $outfile -encoding "utf8"
--- README.md
+++ README.md
@@ -17,21 +17,29 @@ PowerShell スクリプトにて、ユーザー毎に最終サインイン日時
 - サインイン ログの保存期間により、30 日以上前のサインイン日時は確認できません。
 - サインインのログから一覧を取得しているため、厳密な ”最終アクセス日時” とは異なります。
 
-## 最終サインイン日時取得手順
+## 最終サインイン日時の取得手順
 
-### 1. 認証に使用する証明書の作成
+本スクリプトは、キーまたは証明書を利用して最終サインイン日時を取得します。
+キーは事前準備が容易であるため、一時的な検証に向いています。
+証明書の場合、証明書を作成する事前準備が必要となりますがキーよりも安全であり推奨される方法です。
+
+
+### 証明書を利用する場合は予め以下の準備が必要です。
+- 認証に使用する証明書の作成
+
 CreateAndExportCert.ps1 を実行します。
 
-最終サインイン日時の取得にトークン取得に証明書を用います。これは、これまでの平文のキーを用いる方法よりもセキュリティ的に強固であり、推奨している方法です。CreateAndExportCert.ps1 は自己署名証明書を生成し、ユーザーの証明書ストア (個人) に格納します。さらに、公開鍵を含む証明書 (SelfSignedCert.cer ファイル) をカレント  ディレクトリに出力します。
+CreateAndExportCert.ps1 は自己署名証明書を生成し、ユーザーの証明書ストア (個人) に格納します。さらに、公開鍵を含む証明書 (SelfSignedCert.cer ファイル) をカレント  ディレクトリに出力します。
 
-### 2. 処理に必要なライブラリを nuget で取得するスクリプトの準備と実行
+- 処理に必要なライブラリを nuget で取得するスクリプトの準備と実行
+
 GetModuleByNuget.ps1 を C:\SignInReport フォルダー配下に保存し実行します。
 
 証明書を用いたトークン取得処理に必要なライブラリを nuget で取得します。
 GetModuleByNuget.ps1 を実行すると、C:\SignInReport 配下に Tools というフォルダーが作成され Microsoft.IdentityModel.Clients.ActiveDirectory.dll などのファイルが保存されます。
 
  
-### 3. アプリケーションの登録
+### 1.アプリケーションの登録
 Azure AD 上にアプリケーションを準備します。
 
 以下のドキュメントに記載された手順に従って、アプリケーションを登録し、"構成設定を収集する" に従ってドメイン名とクライアント ID を取得します。
@@ -40,21 +48,37 @@ Azure AD Reporting API にアクセスするための前提条件
 https://docs.microsoft.com/ja-jp/azure/active-directory/active-directory-reporting-api-prerequisites-azure-portal
 
 
-今回はアプリケーションのクライアント シークレットとして証明書を利用します。そのため、 上記の公開情報では "アプリケーションのクライアント シークレットを取得する" の手順において 5 でキーを生成していますが、その代わりに [公開キーのアップロード] を実行します。ここで、 "1. 認証に使用する証明書の作成" で作成した、 SelfSignedCert.cer を指定してアップロード後、 [保存] をクリックします。
+キーを利用する場合：上記の公開情報の "アプリケーションのクライアント シークレットを取得する" の、5 の手順においてキーを作成します。
+
+![キーの作成画面](img/createkey.png)
+
+証明書を利用する場合：
+上記の公開情報の "アプリケーションのクライアント シークレットを取得する" の、5 の手順においてキーを作成する代わりに、[公開キーのアップロード] を実行します。ここで、 "1. 認証に使用する証明書の作成" で作成した、 SelfSignedCert.cer を指定してアップロード後、 [保存] をクリックします。
 
  ![クライアント シークレットの証明書アップロード画面](img/certupload.png)
 
-### 4. スクリプトの実行
+### 2. スクリプトの実行
 
 最後に、GetLastLogin.ps1 を C:\SignInReport 配下に保存し、環境に合わせて以下部分の内容を設定します。
 
 ```powershell
 $tenantId = "yourtenant.onmicrosoft.com" # or GUID "01234567-89AB-CDEF-0123-456789ABCDEF"
 $clientID = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXX"
-$thumprint = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
 $outfile = "C:\Users\xxxxx\Desktop\lastLogin.csv"
 ```
 
+引数にて、キーまたは証明書を選択します。
+
+キーの場合：
+```powershell
+.\GetLastLogin.ps1 -authMethod Key <手順1で作成したキーの値>
+```
+証明書の場合：
+```powershell
+.\GetLastLogin.ps1 -authMethod Cert <手順1でアップロードした証明書の拇印の値>
+```
+
+### 実行結果
 GetLastLogin.ps1 を実行すると、ユーザー毎に最終サインイン日時が csv ファイルとして取得できます。
 
 
